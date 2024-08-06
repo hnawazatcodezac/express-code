6b420ca18aab4726868d5e96edb57dc5e295fd0e
Hamid Nawaz <hamid.nawaz@codezac.com>
2024-08-06 16:10:31 +0500

    COD-in-main

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
 app.use(`/`, handler);
 
 function handler(req, res) {
-  res.status(200).end('Hello World In main Hel!');
+  res.status(200).end('Hello World In main Hello!');
 }
 
 app.listen(port, () => {
