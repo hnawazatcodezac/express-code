e857f67c402c568c5b76dd67a204a5043fddbf28
Hamid Nawaz <hamid.nawaz@codezac.com>
2024-08-06 15:31:58 +0500

    added route

---
+++ index.js
@@ -0,0 +1,23 @@
+import cors from "cors";
+import express from "express";
+
+const port = 3001;
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(`/`, handler);
+
+function handler(req, res) {
+  res.status(200).end('Hello World!');
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
